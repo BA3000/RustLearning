918a03e7dc652c526ece9c6d459e101b7c7b501e
BA3000 <2864281096@qq.com>
2021-11-24 23:24:25 +0800

    finish the guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,45 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    // [1, 101), get random number
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // shadow the previously defined variable "guess"
+        // trim() for eliminating spaces in the beginning and end, also removes \n, \r, or \r\n etc.
+        // parse() is for parsing a string into some kind of number. target type is required, you can
+        // specify type by using var_name: type_name like below.
+        // expect() for catching errors
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid num!");
+                continue;
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        // values to be compared must have the same type
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
